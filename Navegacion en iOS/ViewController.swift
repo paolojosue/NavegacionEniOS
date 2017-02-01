457c3645526516eaf6b7df49eba290e8fbb5e081
Alumno <alumno@admins-macbook-pro.local>
2017-01-31 21:17:48 -0500

    Navegación en iOS

--- Navegacion en iOS/ViewController.swift
+++ Navegacion en iOS/ViewController.swift
@@ -20,6 +20,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnNavegacionCodigoTouch(_ sender: UIButton) {
+        
+        self.performSegue(withIdentifier: "transicion", sender:sender)
+        
+        
+    }
 
 }
 
